0f3560a719d8404432789876cb7e0b6a77ceec19
Saro0h <mkhalil.sarah@gmail.com>
2014-10-29 17:00:37 +0100

    Initial commit

---
+++ src/Saro0h/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Saro0h\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/talks", name="talks")
+     * @Template()
+     */
+    public function talksAction()
+    {
+        return array();
+    }
+}
