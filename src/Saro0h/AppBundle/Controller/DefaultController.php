f1e214e4561049388f8773b0190e46a5c7ece4c1
Saro0h <mkhalil.sarah@gmail.com>
2014-10-29 19:05:12 +0100

    Added articles about code

--- src/Saro0h/AppBundle/Controller/DefaultController.php
+++ src/Saro0h/AppBundle/Controller/DefaultController.php
@@ -25,4 +25,58 @@ class DefaultController extends Controller
     {
         return array();
     }
+
+    /**
+     * @Route("/code", name="code")
+     * @Template()
+     */
+    public function codeAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/kata-translation", name="kata_translation")
+     * @Template()
+     */
+    public function kataTranslationAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/playground-rabbitmq", name="playground_rabbitmq")
+     * @Template()
+     */
+    public function playgroundRabbitMQAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/kata-upload", name="kata_upload")
+     * @Template()
+     */
+    public function uploadAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/kata-event-listener", name="kata_event_listener")
+     * @Template()
+     */
+    public function eventListenerAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/playground-media-api-bundle", name="playground_media_api")
+     * @Template()
+     */
+    public function playgroundMediaAction()
+    {
+        return array();
+    }
 }
